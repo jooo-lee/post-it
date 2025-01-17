71e813f29cc6c3169f3d7b4c284bc705d87365fb
Joseph Lee <josephlee3004@gmail.com>
2025-01-17 10:45:02 -0500

    Display messages using index template

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('node:path');
+
 const indexRouter = require('./routes/indexRouter');
 const messageRouter = require('./routes/messageRouter');
 
 const app = express();
 const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
 app.use('/new', messageRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,26 @@
 const { Router } = require('express');
 
 const indexRouter = Router();
+const messages = [
+  {
+    text: 'Roger roger.',
+    user: 'Battle droid',
+    added: new Date(),
+  },
+  {
+    text: 'Sir, yes sir!',
+    user: 'Clone trooper',
+    added: new Date(),
+  },
+  {
+    text: 'Do or do not, there is no try.',
+    user: 'Yoda',
+    added: new Date(),
+  },
+];
 
 indexRouter.get('/', (req, res) => {
-  res.send('index');
+  res.render('index', { messages: messages });
 });
 
 module.exports = indexRouter;
