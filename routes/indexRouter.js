14221c340c095d0a402a472422bdbd5b7adaf800
Joseph Lee <josephlee3004@gmail.com>
2025-01-17 11:58:46 -0500

    Create new message form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -23,7 +23,12 @@ indexRouter.get('/', (req, res) => {
   res.render('index', { messages: messages });
 });
 indexRouter.get('/new', (req, res) => {
-  res.send('new');
+  res.render('form');
+});
+indexRouter.post('/new', (req, res) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = indexRouter;
